5f2a2c612e6286430013646b1eef4b47d8387c68
Baptiste Mille-Mathias <baptiste.millemathias@gmail.com>
2011-10-03 23:14:07 +0200

    Refactor code

--- displayHeader.py
+++ displayHeader.py
@@ -3,80 +3,79 @@
 import sys
 from gi.repository import Soup, SoupGNOME, Gtk
 
-#if len(sys.argv) < 2:
- #   sys.exit(1)
+class displayHeader(Gtk.Window):
 
-#URL = sys.argv[1]
+    def __init__(self):
+        super(displayHeader, self).__init__()
 
-class displayHeader():
+        global session
+        session = Soup.SessionAsync.new()
 
-    def clicked_button_action (button,entry):
+        Logger = Soup.Logger.new(Soup.LoggerLogLevel.HEADERS, -1)
+        Logger.set_printer(self.add_log_to_treeview, None)
+
+        CookiesJar = Soup.CookieJarText.new("cookies.txt", False)
+        session.add_feature_by_type(SoupGNOME.ProxyResolverGNOME)
+        session.add_feature(Logger)
+        session.add_feature(CookiesJar)
+
+        self.connect("delete-event", Gtk.main_quit)
+        self.set_size_request(340, 400)
+        self.set_border_width(10)
+
+        hbox = Gtk.Box()
+        hbox.set_spacing(6)
+        hbox.set_orientation(Gtk.Orientation.HORIZONTAL)
+
+        vbox = Gtk.Box()
+        vbox.set_spacing(6)
+        vbox.set_orientation(Gtk.Orientation.VERTICAL)
+        vbox.pack_start(hbox, False, True, 6)
+        self.add(vbox)
+
+        global entry 
+        entry = Gtk.Entry()
+        entry.connect("activate", self.clicked_button_action)
+        button= Gtk.Button.new_from_stock(Gtk.STOCK_OK)
+        button.connect("clicked", self.clicked_button_action)
+        hbox.pack_start(entry, True, True, 0)
+        hbox.pack_start(button, False, False, 0)
+
+        scrollwidget = Gtk.ScrolledWindow()
+        scrollwidget.set_shadow_type(Gtk.ShadowType.IN)
+        vbox.pack_start(scrollwidget, True, True, 0)
+
+        global textview
+        textview = Gtk.TextView()
+        textview.set_editable(False)
+        scrollwidget.add(textview)
+
+        self.show_all()
+
+    def add_log_to_treeview (logger, printer, direction, header, *args):
+        textbuffer=textview.get_buffer()
+        textbuffer.set_text(textbuffer.get_text(textbuffer.get_start_iter(),
+                                                textbuffer.get_end_iter(),
+                                                False)
+                                                 +  "%c %s \n"%(direction, header))
+        textview.set_buffer(textbuffer)
+
+    def clicked_button_action (self, *args):
         url = entry.get_text()
         if len(url) == 0:
                 return
         if not url.startswith('http://'):
                 url = "http://"+ url
         message = Soup.Message.new('GET', url)
-        session.send_message(message)
+        session.queue_message(message, self.add_log_to_treeview, *args)
 
         if message.status_code == '200':
                 entry.set_text('')
-                
+        entry.set_text('')
+
         print message.status_code
         print message.reason_phrase
 
-    def add_log_to_treeview (logger, printer, data, laa, loo):
-        textbuffer=textview.get_buffer()
-        textbuffer.set_text(textbuffer.get_text(textbuffer.get_start_iter(),
-                                                textbuffer.get_end_iter(),
-                                                False)
-                                                 +  "%c %s \n"%(data, laa))
-        textview.set_buffer(textbuffer)
-
-    global session
-    session = Soup.SessionSync.new()
-
-    Logger = Soup.Logger.new(Soup.LoggerLogLevel.HEADERS, -1)
-    Logger.set_printer(add_log_to_treeview, None)
-
-    CookiesJar = Soup.CookieJarText.new("cookies.txt", False)
-    session.add_feature_by_type(SoupGNOME.ProxyResolverGNOME)
-    session.add_feature(Logger)
-    session.add_feature(CookiesJar)
-
-    window = Gtk.Window()
-    window.connect("delete-event", Gtk.main_quit)
-    window.set_size_request(340, 400)
-    window.set_border_width(10)
-
-    hbox = Gtk.Box()
-    hbox.set_spacing(6)
-    hbox.set_orientation(Gtk.Orientation.HORIZONTAL)
-
-    vbox = Gtk.Box()
-    vbox.set_spacing(6)
-    vbox.set_orientation(Gtk.Orientation.VERTICAL)
-    vbox.pack_start(hbox, False, True, 6)
-    window.add(vbox)
-
-    entry = Gtk.Entry()
-    button= Gtk.Button.new_from_stock(Gtk.STOCK_OK)
-    button.connect("clicked", clicked_button_action, entry)
-    hbox.pack_start(entry, True, True, 0)
-    hbox.pack_start(button, False, False, 0)
-
-    scrollwidget = Gtk.ScrolledWindow()
-    scrollwidget.set_shadow_type(Gtk.ShadowType.IN)
-    vbox.pack_start(scrollwidget, True, True, 0)
-
-    global textview
-    textview = Gtk.TextView()
-    textview.set_editable(False)
-    scrollwidget.add(textview)
-
-    window.show_all()
-
-    Gtk.main()
-
 if __name__ == '__main__':
     displayHeader()
+    Gtk.main()
